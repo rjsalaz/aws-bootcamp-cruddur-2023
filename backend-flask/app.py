297b764a401223d7164b1f11a2f5b5ac6cc426df
rjsalaz <randysalazar235@yahoo.com>
2023-03-11 12:35:44 -0600

    Added rollbar logging

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -21,7 +21,7 @@ from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
 ##########################################
 
 
-####### CloudWatch #######
+####### CloudWatch Imports  #######
 import watchtower
 import logging
 from time import strftime
@@ -40,7 +40,14 @@ processor = BatchSpanProcessor(OTLPSpanExporter())
 provider.add_span_processor(processor)
 trace.set_tracer_provider(provider)
 tracer = trace.get_tracer(__name__)
-########################################## 
+##########################################
+ 
+############### Rollbar Imports ##################
+import rollbar
+import rollbar.contrib.flask
+from flask import got_request_exception
+##########################################
+
 
 app = Flask(__name__)
 
@@ -70,6 +77,28 @@ cors = CORS(
   allow_headers="content-type,if-modified-since",
   methods="OPTIONS,GET,HEAD,POST"
 )
+
+rollbar_access_token = os.getenv('ROLLBAR_ACCESS_TOKEN')
+@app.before_first_request
+def init_rollbar():
+    """init rollbar module"""
+    rollbar.init(
+        # access token
+        rollbar_access_token,
+        # environment name
+        'development',
+        # server root directory, makes tracebacks prettier
+        root=os.path.dirname(os.path.realpath(__file__)),
+        # flask already sets up logging
+        allow_logging_basic_config=False)
+
+    # send exceptions from `app` to rollbar, using flask's signal system.
+    got_request_exception.connect(rollbar.contrib.flask.report_exception, app)
+
+@app.route('/rollbar/test')
+def rollbar_test():
+    rollbar.report_message('Hello World!', 'warning')
+    return "Hello World!"
 
 @app.route("/api/message_groups", methods=['GET'])
 def data_message_groups():
